5bc5bbc45a34e3cccfccdcde6b2940648e9d282b
Mirjana Orlic <mirjana.orlic4@gmail.com>
2019-03-13 21:11:19 +0100

    changes

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 
-document.getElementById("main-work").addEventListener("click", function() {    
+document.querySelector(".main-work").addEventListener("click", function() {    
     this.classList.toggle("is-active");   
+    remove('.main-bio', '.main-contact');
 });
 
-document.getElementById("main-bio").addEventListener("click", function() {
+document.querySelector(".main-bio").addEventListener("click", function() {
     this.classList.toggle("is-active");
+    remove('.main-work', '.main-contact');
 });
 
-document.getElementById("main-contact").addEventListener("click", function() {
+document.querySelector(".main-contact").addEventListener("click", function() {
     this.classList.toggle("is-active");
+    remove('.main-bio', '.main-work');
 });
 
 function remove(a, b) {
-    document.getElementById(a).classList.remove("is-active");
-    document.getElementById(b).classList.remove("is-active");
+    document.querySelector(a).classList.remove("is-active");
+    document.querySelector(b).classList.remove("is-active");
 }
 
