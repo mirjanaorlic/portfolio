cfb137e83e616e528c2c96459deaa4fb1a5c91ea
Mirjana Orlic <mirjana.orlic4@gmail.com>
2019-03-13 18:23:08 +0100

    changes

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 
-document.getElementById("main__work").addEventListener("click", function() {    
+document.getElementById("main-work").addEventListener("click", function() {    
     this.classList.toggle("is-active");   
 });
 
-document.getElementById("main__bio").addEventListener("click", function() {
+document.getElementById("main-bio").addEventListener("click", function() {
     this.classList.toggle("is-active");
 });
 
-document.getElementById("main__contact").addEventListener("click", function() {
+document.getElementById("main-contact").addEventListener("click", function() {
     this.classList.toggle("is-active");
 });
-
 
 function remove(a, b) {
     document.getElementById(a).classList.remove("is-active");
