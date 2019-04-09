65c609608aecd7ab10181d2547504ef1ea1a1525
Mirjana Orlic <mirjana.orlic4@gmail.com>
2019-04-09 21:47:40 +0200

    changes

--- script.js
+++ script.js
@@ -3,19 +3,22 @@ document.querySelector(".main-work").addEventListener("click", function() {
     this.classList.toggle("is-active");   
     remove('.main-bio', '.main-contact');
 });
-
 document.querySelector(".main-bio").addEventListener("click", function() {
     this.classList.toggle("is-active");
     remove('.main-work', '.main-contact');
 });
-
 document.querySelector(".main-contact").addEventListener("click", function() {
     this.classList.toggle("is-active");
     remove('.main-bio', '.main-work');
 });
-
 function remove(a, b) {
     document.querySelector(a).classList.remove("is-active");
     document.querySelector(b).classList.remove("is-active");
 }
+// Animate
+// var headerName = document.querySelector('.header__name');
+// var header = document.querySelector('.header');
+// header.addEventListener('click', function() {
+//     headerName.classList.add('.animate');
+// });
 
