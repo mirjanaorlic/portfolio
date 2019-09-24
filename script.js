c17356fa85ae486bacb2e781d96d582776d7679e
Mirjana Orlic <mirjana.orlic4@gmail.com>
2019-09-24 11:16:11 +0200

    change background color script added

--- script.js
+++ script.js
@@ -18,19 +18,13 @@ function remove(a, b) {
 
 // Change background color on move
 
+document.addEventListener('click', changeColor); 
 
-// document.addEventListener('onmousemove',changeColor); 
-
-
-// function changeColor() {
-//     var  randcol= "";
-//     var allchar="0123456789ABCDEF";
-//     for(var i = 0; i < 6; i++){
-//          randcol += allchar[Math.floor(Math.random()*16)];
-//       }
-//       document.body.style.backgroundColor= "#"+randcol;
-// };
-
-
-// x = event.clientX;
-// y = event.clientY;
+function changeColor() {
+    var  randcol= "";
+    var allchar="0123456789ABCDEF";
+    for(var i = 0; i < 6; i++){
+         randcol += allchar[Math.floor(Math.random()*16)];
+      }
+      document.body.style.backgroundColor= "#"+randcol;
+};
