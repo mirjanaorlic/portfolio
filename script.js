b2ca475422754ebe864b59bc63facf66d947ad59
Mirjana Orlic <mirjana.orlic4@gmail.com>
2019-11-26 22:23:45 +0100

    improving UI

--- script.js
+++ script.js
@@ -29,3 +29,41 @@ function changeColor() {
       document.body.style.backgroundColor= "#"+randcol;
       console.log(randcol);
 };
+
+
+var rgb = [255, 0, 0];
+
+document.addEventListener('click', changeColor); 
+
+function changeColor() {
+    // randomly update
+    rgb[0] = Math.round(Math.random() * 255);
+    rgb[1] = Math.round(Math.random() * 255);
+    rgb[2] = Math.round(Math.random() * 255);
+  
+    
+    var o = Math.round(((parseInt(rgb[0]) * 299) +
+                        (parseInt(rgb[1]) * 587) +
+                        (parseInt(rgb[2]) * 114)) / 1000);
+    var fore = (o > 125) ? 'black' : 'white';
+    var back = 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')';
+
+    document.body.style.backgroundColor = back;
+    document.body.style.color = fore;
+
+    document.getElementById('text').style.fill = fore;
+    // document.getElementsByClassName('main').style.borderTop = fore;
+    // document.getElementsByClassName('main-work').style.borderRight = fore;
+    // document.getElementsByClassName('main-bio').style.borderRight = fore;
+    // document.links.style.color = fore;
+    var links = document.links;
+    for(var i = 0; i < links.length; i++) {
+        document.links[i].style.color = fore;
+    }
+
+    var githubLogo = document.getElementsByClassName('github-logo');
+    for (var i = 0; i < githubLogo.length; i++) {
+        githubLogo[i].style.fill = fore;
+      }
+    
+  }
